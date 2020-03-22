9fce88c4b93a99adb1cffb81b9948e9c79359e4d
analarajesh <analarajesh2769@gmail.com>
2020-03-22 16:42:17 +0530

    initial commit

---
+++ attendance.php
@@ -0,0 +1,253 @@
+<?php
+$conn=mysqli_connect("localhost","root","");
+$db=mysqli_select_db($conn,"stud");
+if($db)
+{
+echo "connected succesfully";
+}
+else{
+echo "not connected";
+}
+?>
+
+<?php
+    if(isset($_GET['username'])){
+      $usn = $_GET['username'];
+ }else{
+      $usn = "USN not set in GET Method";
+ }
+
+  $query = "SELECT * FROM att where username='$usn';";
+  $result=mysqli_query($conn,$query);
+  $resultcheck=mysqli_num_rows($result);
+
+  ?>
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+#customers {
+  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 80%;
+  margin-top:8%;
+  margin-left:8%;
+}
+
+#customers td, #customers th {
+  border: 1px solid #ddd;
+  padding: 10px;
+}
+
+#customers tr{background-color: #f2f2f2;}
+
+#customers tr:hover {background-color: #ddd;}
+
+#customers th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: rgb(65, 121, 158);
+  color: white;
+}
+
+.b2{
+  position:relative;
+  display: block;
+  padding:8px 5px 2px 5px;
+  color: black;
+  margin: 0 auto;
+  background: snow;
+  font-size: 18px;
+  text-align: center;
+  font-style: normal;
+  width: 15%;
+  border: 1px solid rgb(74, 74, 187);
+  border-width: 2px 2px 5px;
+  margin-bottom:10px;
+  margin-top:5%;
+  margin-left:5%;
+}
+
+.b2:hover
+{
+  background:rgb(133, 193, 221);
+}
+
+.navbar {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.navbar a {
+  float: left;
+  font-size: 16px;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+.dropdown {
+  float: left;
+  overflow: hidden;
+}
+
+.dropdown .dropbtn {
+  font-size: 16px;  
+  border: none;
+  outline: none;
+  color: white;
+  padding: 14px 16px;
+  background-color: inherit;
+  font-family: inherit;
+  margin: 0;
+}
+
+.navbar a:hover, .dropdown:hover .dropbtn {
+  background-color: cornflowerblue	;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  float: none;
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+.dropdown-content a:hover {
+  background-color: #ddd;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+
+
+</style>
+</head>
+<body id="body-color" style="background:url(b4.jpeg); background-repeat:no-repeat;background-size:500%500%">
+
+    <div class="navbar">
+        <a href="login.php">Logout</a> 
+        <a href="icon.php">Back</a>
+        <div class="dropdown">
+          <!-- <button class="dropbtn">View
+            <i class="fa fa-caret-down"></i>
+          </button> -->
+<!--           <div class="dropdown-content">
+            <a href="#">Sem 1</a>
+            <a href="#">Sem 2</a>
+            <a href="#">Sem 3</a>
+            <a href="#">Sem 4</a>
+            <a href="#">Sem 5</a>
+            <a href="#">Sem 6</a>
+            <a href="#">Sem 7</a>
+            <a href="#">Sem 8</a>
+          </div> -->
+        </div> 
+      </div>
+
+    <h1>ATTENDANCE RECORD</h1>
+
+<table id="customers">
+  <tr>
+    <th>Username</th>
+    <th>Subject</th>
+    <th>Classes Held</th>
+    <th>Classes Attended</th>
+    <th>Percentage</th>
+  </tr>
+  
+  <?php
+  
+
+ 
+  if(($resultcheck)>0){
+  while($rows=mysqli_fetch_assoc($result))
+  {
+   ?>
+   
+  <tr>
+    <td><?php echo $rows['username'];?></td>
+    <td><?php echo $rows['sub'];?></td>
+    <td><?php echo $rows['class'];?></td>
+    <td><?php echo $rows['classatt'];?></td>
+    <td><?php echo $rows['percentage'];?></td>
+	<?php
+  }
+  }
+  ?>
+    
+  </tr>
+  <!--<tr>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+  </tr>
+
+  <tr>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+  </tr>
+
+  <tr>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+  </tr>
+
+  <tr>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+  </tr>
+  
+  <tr>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+  </tr>
+
+  <tr>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+  </tr>--!>
+    
+</table>
+<!-- <button  class="b2" type="submit" name="submit" onclick="go()"><b>Back</b></button><br/><br/><br/>
+
+<script>
+  function go()
+  {
+    window.location.assign('icon.html');
+  }
+  </script> -->
+
+</body>
+</html>
---
+++ report.php
@@ -0,0 +1,268 @@
+<?php
+$conn=mysqli_connect("localhost","root","");
+$db=mysqli_select_db($conn,"stud");
+if($db)
+{
+echo "connected succesfully";
+}
+else{
+echo "not connected";
+}
+?>
+
+
+<?php
+    if(isset($_GET['username'])){
+      $usn = $_GET['username'];
+ }else{
+      $usn = "USN not set in GET Method";
+ }
+
+  $query = "SELECT * FROM report where username='$usn';";
+  $result=mysqli_query($conn,$query);
+  $resultcheck=mysqli_num_rows($result);
+
+  ?>
+  
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.navbar {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.navbar a {
+  float: left;
+  font-size: 16px;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+.dropdown {
+  float: left;
+  overflow: hidden;
+}
+
+.dropdown .dropbtn {
+  font-size: 16px;  
+  border: none;
+  outline: none;
+  color: white;
+  padding: 14px 16px;
+  background-color: inherit;
+  font-family: inherit;
+  margin: 0;
+}
+
+.navbar a:hover, .dropdown:hover .dropbtn {
+  background-color: cornflowerblue	;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  float: none;
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+.dropdown-content a:hover {
+  background-color: #ddd;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+#customers {
+  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 80%;
+  margin-top:8%;
+  margin-left:8%;
+}
+
+#customers td, #customers th {
+  border: 1px solid #ddd;
+  padding: 10px;
+}
+
+/* #customers tr:nth-child(even){background-color: #f2f2f2;} */
+#customers tr{background-color: #f2f2f2;}
+
+#customers tr:hover {background-color: #ddd;}
+
+#customers th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: rgb(65, 121, 158);
+  color: white;
+}
+
+
+
+
+/* table, td, th {  
+  /* border: 1px solid #ddd; */
+  /* text-align: center;
+  border: 1px solid black;
+}
+table {
+  border-collapse: collapse;
+  width: 100%;
+  margin-top:100px;
+ 
+}
+th, td {
+  padding: 15px;
+} */ 
+
+</style>
+</head>
+<body>
+    <body id="body-color" style="background:url(b4.jpeg); background-repeat:no-repeat;background-size:500% 500%" ></body>
+    <div class="navbar">
+        <a href="login.php">Logout</a> 
+        <a href="icon.php">Back</a>
+        <div class="dropdown">
+          <button class="dropbtn">View
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content">
+            <a href="#">Sem 1</a>
+            <a href="#">Sem 2</a>
+            <a href="#">Sem 3</a>
+            <a href="#">Sem 4</a>
+            <a href="#">Sem 5</a>
+            <a href="#">Sem 6</a>
+            <a href="#">Sem 7</a>
+            <a href="#">Sem 8</a>
+          </div>
+        </div> 
+      </div>
+      
+<h2>SEMESTER END EXAM RESULTS</h2>
+
+
+<table id="customers">
+  <tr>
+    <th>COURSE NAME</th>
+    <th>COURSE CODE</th>
+    <th>CIE</th>
+    <th>SEE</th>
+    <th>GRADE</th>
+    <th>GRADE POINT</th>
+  </tr>
+  <?php
+  
+
+ 
+  if(($resultcheck)>0){
+  while($rows=mysqli_fetch_assoc($result))
+  {
+   ?>
+   
+  <tr>
+    <td><?php echo $rows['username'];?></td>
+    <td><?php echo $rows['code'];?></td>
+    <td><?php echo $rows['cie'];?></td>
+    <td><?php echo $rows['see'];?></td>
+    <td><?php echo $rows['grade'];?></td>
+    <td><?php echo $rows['gradepoint'];?></td>
+	<?php
+  }
+  }
+  ?>
+    
+  </tr>
+
+ <!-- <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+   
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    
+  </tr>-->
+  
+
+</table>
+
+</body>
+</html>
